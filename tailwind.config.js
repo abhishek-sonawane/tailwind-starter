a089477fc16204746fdffcbac91fddf89b8ac955
abhishek <abhisheksonwane57@gmail.com>
2022-11-22 18:17:03 +0530

    configs and template made in tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+   
+    extend: {
+      colors:{
+        twitch:'#4f46e5',
+        twitch_light:'#6366f1'
+      },
+    },
+  },
+  plugins: [],
+}
