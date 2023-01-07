b7c528383df8395f2cdb5ae9cac2db2856c14da7
abhishek <abhisheksonwane57@gmail.com>
2023-01-07 19:03:55 +0530

    style changes & package.json updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,44 @@ module.exports = {
         twitch:'#4f46e5',
         twitch_light:'#6366f1'
       },
+      'animation': {
+        'gradient-x':'gradient-x 7s ease infinite',
+        'gradient-y':'gradient-y 15s ease infinite',
+        'gradient-xy':'gradient-xy 15s ease infinite',
+    },
+    'keyframes': {
+        'gradient-y': {
+            '0%, 100%': {
+                'background-size':'400% 400%',
+                'background-position': 'center top'
+            },
+            '50%': {
+                'background-size':'200% 200%',
+                'background-position': 'center center'
+            }
+        },
+        'gradient-x': {
+            '0%, 100%': {
+                'background-size':'200% 200%',
+                'background-position': 'left center'
+            },
+            '50%': {
+                'background-size':'200% 200%',
+                'background-position': 'right center'
+            }
+        },
+        'gradient-xy': {
+            '0%, 100%': {
+                'background-size':'400% 400%',
+                'background-position': 'left center'
+            },
+            '50%': {
+                'background-size':'200% 200%',
+                'background-position': 'right center'
+            }
+        }
+    }
+      
     },
   },
   plugins: [],
